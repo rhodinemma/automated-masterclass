b1423508e06ca8e5815358ca410276027c1c8ef6
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-07-15 08:25:41 +0300

    fix deprecated methods

--- src/test/java/com/seldem/seldem/App.java
+++ src/test/java/com/seldem/seldem/App.java
@@ -3,14 +3,15 @@ package com.seldem.seldem;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class App {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.cranecloud.io");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://cranecloud.io");
         driver.close();
     }
 }
