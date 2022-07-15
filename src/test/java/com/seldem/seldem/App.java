cb497b8de3559cb85f44b914efb9110d16874554
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-07-15 15:09:05 +0300

    using locators to retrieve web elements

--- src/test/java/com/seldem/seldem/App.java
+++ src/test/java/com/seldem/seldem/App.java
@@ -1,17 +1,41 @@
 package com.seldem.seldem;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 public class App {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver","chromedriver.exe");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.get("https://cranecloud.io");
+        driver.get("https://www.wikipedia.org/");
+
+        // Using Locators in Selenium
+
+        // id
+        long start = System.currentTimeMillis();
+        driver.findElement(By.id("js-link-box-en"));
+        long end = System.currentTimeMillis();
+
+        System.out.println("Time spent " + (end-start));
+
+        // xpath
+        start = System.currentTimeMillis();
+        driver.findElement(By.xpath("//*[@id=\"www-wikipedia-org\"]/div[2]/div[2]"));
+        end = System.currentTimeMillis();
+
+        System.out.println("Time 2 spent " + (end-start));
+
+        // css selector
+        start = System.currentTimeMillis();
+        driver.findElement(By.cssSelector("#www-wikipedia-org > div.central-featured > div.central-featured-lang.lang2"));
+        end = System.currentTimeMillis();
+
+        System.out.println("Time 3 spent " + (end-start));
+
         driver.close();
     }
 }
